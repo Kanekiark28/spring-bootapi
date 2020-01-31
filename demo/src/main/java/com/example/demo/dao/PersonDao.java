535c8732b86905d11e753ec6cf39361bb2c00f73
Kanekiark28 <konarkrajmishra@gmail.com>
2020-01-30 22:45:03 -0800

    api dev complete tree

---
+++ demo/src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,27 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    //insert a person into database
+    int insertPerson(UUID id,Person person);
+
+    default int insertPerson(Person person){
+        //generate UUIDs ourselves
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+}
